e7690eac434aa21861866b5569964d2b9fe4f520
vargasgonzalo848 <163219761+vargasgonzalo848@users.noreply.github.com>
2025-10-27 11:19:05 -0300

    Add files via upload

--- odontologia/dentistas/crear.php
+++ odontologia/dentistas/crear.php
@@ -109,5 +109,5 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   <button type="submit" class="btn btn-primary">Guardar</button>
   <a href="lista.php" class="btn btn-secondary">Cancelar</a>
 </form>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <?php include("../includes/footer.php"); ?>
